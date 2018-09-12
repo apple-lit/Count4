5f71d26851ccb84648ce3f4d1306fbfe9a4075d1
apple-lit <rintaro.kudo@gmail.com>
2018-09-12 15:27:47 +0900

    改行を入れた

--- Count4/ViewController.swift
+++ Count4/ViewController.swift
@@ -9,6 +9,10 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var number : Int = 0
+    @IBOutlet var countLabel : UILabel!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,6 +22,11 @@ class ViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func plus(){
+        number += 1
+        countLabel.text = String(number)
     }
 
 
